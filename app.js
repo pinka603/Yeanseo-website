024669552d4fd1e439c641435045ea6302256e1a
pinka603 <95180276+pinka603@users.noreply.github.com>
2022-03-28 22:18:58 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const mon = "mon";
+const tue = "tue"; 
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund"; 
+
+const week= mon + tue + wed + thu + fri +sat + sun;
+console.log( Week);
